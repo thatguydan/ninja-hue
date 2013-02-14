1175f5fdba616e2cff5c87d4fcbdedda109f3d2c
Daniel Friedman <danielnfriedman@gmail.com>
2013-02-14 16:23:04 +1100

    Work on config

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var util = require('util')
   , os = require('os')
   , helpers = require('./lib/helpers')
   , Hue = require('hue.js')
-  , Light = require('./lib/Light');
+  , Light = require('./lib/Light')
+  , configHandlers = require('./lib/config');
 
 util.inherits(hue,stream);
 module.exports = hue;
@@ -22,19 +23,44 @@ function hue(opts,app) {
   app.on('client::up', function() {
 
     if (self._opts.stations.length>0) {
-      loadStations.call(self);
+      self.loadStations.call(self);
     } else {
-      findStations.call(self);
+      self.findStations.call(self);
     }
   });
 };
 
-hue.prototype.config = function(config) {
-  // Handle config, then
-  // this.emit('config')
+var HUE_ANNOUNCEMENT = {
+  "contents": [
+    { "type": "heading",      "text": "New Philips Hue Link Detected" },
+    { "type": "paragraph",    "text": "To enable your Hue lights on the dashboard please press the link button on your Hue base station." },
+  ],
+  "actions": []
+};
+/**
+ * Called when a user prompts a configuration
+ * @param  {Object}   payload The return payload from the UI
+ * @param  {String}   rpc     Used to match up requests.
+ * @param  {Function} cb      Callback with return data
+ */
+hue.prototype.config = function(rpc,cb) {
+  console.log(rpc);
+  var self = this;
+
+  if (!rpc) {
+    return configHandlers.probe.call(this,cb);
+  }
+
+  switch (rpc.method) {
+    case 'manual_get_ip':  return configHandlers.manual_get_ip.call(this,rpc.params,cb); break;
+    case 'manual_set_ip':  return configHandlers.manual_set_ip.call(this,rpc.params,cb); break;
+    case 'search':         return configHandlers.search.call(this,rpc.params,cb);        break;
+    default:               return cb(true);                                              break;
+  }
 };
 
-function findStations() {
+
+hue.prototype.findStations = function() {
   this._app.log.info('Hue: No configuration')
   var self = this;
 
@@ -42,12 +68,23 @@ function findStations() {
   if (!this._opts.autoRegister) return;
 
   Hue.discover(function(stations) {
-    stations.forEach(registerStation.bind(self));
-  });
+    stations.forEach(function(station) {
 
+      // If we have already registered this
+      if (self._opts.stations.indexOf(station)>-1) return;
+
+      // If we have already announced this
+      if (self._opts.sentAnnouncements.indexOf(station) > -1) return;
+
+      self.emit('announcement',HUE_ANNOUNCEMENT);
+      self._opts.sentAnnouncements.push(station);
+      self.save();
+      self.registerStation(station);
+    });
+  });
 };
 
-function registerStation(station) {
+hue.prototype.registerStation = function(station,cb) {
 
   if (this._opts.stations.indexOf(station)>-1) {
     // We already have this station registered.
@@ -73,7 +110,8 @@ function registerStation(station) {
 
     if (err) {
       // Do nothing?
-      self._app.log.info('Timed out waiting for station')
+      self._app.log.info('Timed out waiting for station');
+      cb(err);
       return;
     }
 
@@ -82,16 +120,17 @@ function registerStation(station) {
 
     self.save();
 
-    loadStations.call(self);
+    self.loadStations();
+    cb(null)
   });
 };
 
-function loadStations() {
+hue.prototype.loadStations = function() {
 
-  this._opts.stations.forEach(fetchLights.bind(this));
+  this._opts.stations.forEach(this.fetchLights);
 };
 
-function fetchLights(station,stationIndex) {
+hue.prototype.fetchLights = function(station,stationIndex) {
 
   var self = this;
 
